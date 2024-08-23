cd6e8224f02d39b6874b96bdd6d1dfbc6d31f1ff
OscarAcademlo <oscarns@gmail.com>
2024-08-23 17:30:18 -0300

    solucion tema 2 msj

--- app.js
+++ app.js
@@ -4,62 +4,26 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const JsonFileAdapter = require('@bot-whatsapp/database/json')
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
 
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
 
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
 
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
 
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null,
-    [flowSecundario]
-)
+
+
+
 
 const flowPrincipal = addKeyword(EVENTS.WELCOME)
     .addAnswer('🙌 Hola bienvenido al chatbot del sanatorio San Carlos')
     .addAnswer(
         [
-            'Para pedir un turno sin operadora humana ir a https://www.ssancarlos.com.ar/clinica/turnos/',
+            '👉 Para pedir un turno sin operadora humana ir a https://www.ssancarlos.com.ar/clinica/turnos/',
             '👉 Para pedir un turno con operadora ir a etse link http://turnosssc.oscarsoft.me/ ',
             '👉 Para cancelar un turno hacer click aqui https://wa.me/5492944651381',
             '👉 Gracias por contactar con el sanatorio',
         ],
         null,
         null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
+       
     )
 
 const main = async () => {
